4641893a487e7cadd71104d169e09b406bc1f0c2
Farhad-Tanveee <farhadtanveer05u@example.com>
2023-05-18 20:27:31 +0200

    added get method to see all data

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ async function run() {
 
     const toyCollection = client.db("toyPlace").collection("allToys");
 
+    app.get("/allToys", async (req, res) => {
+      const result = await toyCollection.find({}).toArray();
+      res.send(result);
+    });
+
     app.post("/addToy", async (req, res) => {
       const body = req.body;
       const result = await toyCollection.insertOne(body);
